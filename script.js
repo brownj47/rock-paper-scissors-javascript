6dbd1a3d7d9e801ca02d7653d2aeac91fe6b54cd
Justus B <justus.c.brown@gmail.com>
2022-06-23 15:22:41 -0700

    tried to epit some of the logic, didn't get very far

--- script.js
+++ script.js
@@ -4,10 +4,23 @@
 
 var randNum = (Math.floor(Math.random() * 3))
 
-if (randNum === 0) {
-    alert("Rock")
-} else if (randNum === 1){
-    alert("Paper")
-} else {
-    alert("Scissors")
+if (confirm("Are you ready to play?")) {
+
+    if (prompt("Rock, Paper, or Scissors").toLowerCase == "R") {
+        console.log("yey")
+        if (randNum === 0) {
+            alert("R")
+        } else if (randNum === 1){
+            alert("P")
+        } else {
+            alert("S")
+        }
+    }
+
 }
+
+
+
+
+
+
