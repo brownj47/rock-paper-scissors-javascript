38499023f6ba4137623cefd46508326deae62d75
Justus B <justus.c.brown@gmail.com>
2022-06-23 14:03:10 -0700

    added randomizer logic

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 // confirm("Are you ready to play?")
 
 var randNum = (Math.floor(Math.random() * 3))
+
+if (randNum === 0) {
+    alert("Rock")
+} else if (randNum === 1){
+    alert("Paper")
+} else {
+    alert("Scissors")
+}
