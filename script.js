b0c6c543d4d854ca698dcde666cbdac07c662955
Justus B <justus.c.brown@gmail.com>
2022-06-24 11:04:12 -0700

    made everyting into a function

--- script.js
+++ script.js
@@ -2,25 +2,67 @@
 
 // confirm("Are you ready to play?")
 
-var randNum = (Math.floor(Math.random() * 3))
 
-if (confirm("Are you ready to play?")) {
+var RPS = ["R", "P", "S"]
+var randNum = (Math.floor(Math.random() * RPS.length))
+var compChoice = RPS[randNum]
+var humanChoice;
+var tieCount = 0
+var winCount = 0
+var lossCount = 0
 
-    if (prompt("Rock, Paper, or Scissors").toLowerCase == "R") {
-        console.log("yey")
-        if (randNum === 0) {
-            alert("R")
-        } else if (randNum === 1){
-            alert("P")
-        } else {
-            alert("S")
-        }
+function startgame() {
+    if (confirm("Click OK to play")) {
+        humanChoice = prompt("R, P, or S?")
+        humanChoice = humanChoice.toUpperCase()
+        winorlose()
+        scoreboard()
+        startgame()
     }
 
+
+}
+
+function winorlose(){
+
+    if (humanChoice == compChoice) {
+        alert("tie")
+        tieCount = tieCount +1
+    } else if (humanChoice == "R"){
+        if (compChoice == "S") {
+            alert("VICTORY")
+            winCount = winCount +1
+        } else {
+            alert("DEFEAT")
+            lossCount = lossCount +1
+        }
+    } else if (humanChoice == "P"){
+        if (compChoice == "R") {
+            alert("VICTORY")
+            winCount = winCount +1
+        } else {
+            alert("DEFEAT")
+            lossCount = lossCount +1
+        }
+    } else if (humanChoice == "S"){
+        if (compChoice == "P") {
+            alert("VICTORY")
+            winCount = winCount +1
+        } else {
+            alert("DEFEAT")
+            lossCount = lossCount +1
+        }
+    }
+}
+
+function scoreboard() {
+    alert(`Score:
+    Wins: ${winCount}
+    Losses: ${lossCount}
+    Ties: ${tieCount}`)
 }
 
 
 
-
-
+startgame()
 
